af53bf1952226e7631879e1881ca9df7f6984174
unknown <leyou@hogar.(none)>
2012-07-01 15:41:32 +0200

    test

---
+++ sql/database.sql
@@ -0,0 +1,9 @@
+The term 'mysqldump' is not recognized as the name of a cmdlet, function, scrip
+t file, or operable program. Check the spelling of the name, or if a path was i
+ncluded, verify that the path is correct and try again.
+At line:1 char:10
++ mysqldump <<<<  -u root -p coxis3
+    + CategoryInfo          : ObjectNotFound: (mysqldump:String) [], CommandNo 
+   tFoundException
+    + FullyQualifiedErrorId : CommandNotFoundException
+ 
