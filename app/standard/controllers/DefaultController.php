9aa35d41d7358cc10b18e7143b11b649ce394f2b
Michel <leyou.m@gmail.com>
2012-09-05 16:15:53 +0200

    standard bundle

---
+++ app/standard/controllers/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+namespace Coxis\App\Standard\Controllers;
+
+class DefaultController extends \Coxis\Core\Controller {
+	/**
+	@Route('')
+	*/
+	public function indexAction($request) {
+		echo 'Home';
+	}
+
+	public function _404Action($request) {
+	}
+	
+	public function layoutAction($content) {
+		$this->content = $content;
+		$this->view = 'layout.php';
+	}
+}
