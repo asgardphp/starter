f353b41415eea05b6c9e493198f54e8b42574ef8
Michel <leyou.m@gmail.com>
2012-12-05 13:25:28 +0100

    debug

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-//~ define('_ENV_', 'dev');
+// define('_ENV_', '');
+
+if(!defined('_ENV_'))
+	if(isset($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] == '127.0.0.1' || $_SERVER['HTTP_HOST'] == 'localhost'))
+		define('_ENV_', 'dev');
+	else
+		define('_ENV_', 'prod');
 
 /* INIT */
 require('coxis.php');
