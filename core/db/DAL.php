934fb3e40186b63b88d44052bdaaee76009e6b91
Michel <leyou.m@gmail.com>
2012-12-08 14:31:19 +0100

    refactoring dal

--- core/db/DAL.php
+++ core/db/DAL.php
@@ -57,29 +57,15 @@ class DAL {
 		return $this->rsc->next();
 	}
 	
-	public function innerjoin($jointures) {
-		$this->innerjoin = array_merge($this->innerjoin, $jointures);
-		
-		return $this;
-	}
-	
-	public function rightjoin($jointures) {
-		$this->rightjoin = array_merge($this->rightjoin, $jointures);
-		
-		return $this;
-	}
-	
-	public function leftjoin($jointures) {
-		$this->leftjoin = array_merge($this->leftjoin, $jointures);
-		
-		return $this;
-	}
-	
 	public function reset() {
 		$this->where = null;
 		$this->offset = null;
 		$this->limit = null;
-		#todo leftjoin etc.
+		$this->orderBy = null;
+		$this->groupBy = null;
+		$this->leftjoin = array();
+		$this->rightjoin = array();
+		$this->innerjoin = array();
 		
 		return $this;
 	}
@@ -87,37 +73,27 @@ class DAL {
 	public function query($sql, $args) {
 		return $this->db->query($sql);
 	}
-		
-	public function where($conditions) {
-		if($this->where === null)
-			$this->where = array();
-			
-		if(!$conditions)
-			return $this;
-		
-		$this->where[] = static::parseConditions($conditions);
+	
+	/* GETTERS */
+	public function first() {
+		list($sql, $params) = $this->limit(1)->buildSQL();
+		return $this->db->query($sql, $params)->first();
+	}
+	
+	public function get() {
+		list($sql, $params) = $this->buildSQL();
+		return $this->db->query($sql, $params, $params)->all();
+	}
+	
+	public function paginate($page, $per_page=10) {
+		$page = $page ? $page:1;
+		$this->offset(($page-1)*$per_page);
+		$this->limit($per_page);
 		
 		return $this;
 	}
-	
-	private static function parseConditions($conditions) {
-		$res = array();
 
-		if(is_array($conditions)) {
-			foreach($conditions as $k=>$v)
-				if(is_int($k))
-					$res[] = static::parseConditions($v);
-				else {
-					$ar = array();
-					$ar[$k] = static::parseConditions($v);
-					$res[] = $ar;
-				}
-			return $res;
-		}
-		else
-			return $conditions;
-	}
-		
+	/* SETTERS */
 	public function offset($offset) {
 		$this->offset = $offset;
 		return $this;
@@ -137,7 +113,35 @@ class DAL {
 		$this->groupBy = $groupBy;
 		return $this;
 	}
+		
+	public function where($conditions) {
+		if($this->where === null)
+			$this->where = array();
+			
+		if(!$conditions)
+			return $this;
+		
+		$this->where[] = static::parseConditions($conditions);
+		
+		return $this;
+	}
 	
+	public function innerjoin($jointures) {
+		$this->innerjoin = array_merge($this->innerjoin, $jointures);
+		return $this;
+	}
+	
+	public function rightjoin($jointures) {
+		$this->rightjoin = array_merge($this->rightjoin, $jointures);
+		return $this;
+	}
+	
+	public function leftjoin($jointures) {
+		$this->leftjoin = array_merge($this->leftjoin, $jointures);
+		return $this;
+	}
+
+	/* CONDITIONS PROCESSING */
 	private static function processConditions($params, $condition = 'and', $brackets=false, $table=null) {
 		if(sizeof($params) == 0)
 			return array('', array());
@@ -196,6 +200,24 @@ class DAL {
 		return $condition;
 	}
 	
+	private static function parseConditions($conditions) {
+		$res = array();
+
+		if(is_array($conditions)) {
+			foreach($conditions as $k=>$v)
+				if(is_int($k))
+					$res[] = static::parseConditions($v);
+				else {
+					$ar = array();
+					$ar[$k] = static::parseConditions($v);
+					$res[] = $ar;
+				}
+			return $res;
+		}
+		else
+			return $conditions;
+	}
+	
 	public function getTable($ref) {
 		foreach($this->tables as $table=>$alias)
 			if($alias == $ref)
@@ -203,70 +225,54 @@ class DAL {
 		return null;
 	}
 	
-	public function buildSQL() {
-		$where = '';
-		$orderBy = '';
-		$groupBy = '';
-		$leftjoin = '';
-		$rightjoin = '';
-		$innerjoin = '';
-		$limit = null;
-
-		$params = array();
-		
+	/* BUILDERS */
+	public function buildTables() {
 		$tables = array();
 		foreach($this->tables as $table=>$alias)
 			if($alias)
 				$tables[] = '`'.$table.'` '.$alias;
 			else
 				$tables[] = '`'.$table.'`';
-		$sqltable = implode(', ', $tables);
-		
+		return implode(', ', $tables);
+	}
+	public function getDefaultTable() {
 		if(get(array_values($this->tables), 0))
-			$default = get(array_values($this->tables), 0);
+			return get(array_values($this->tables), 0);
 		else
-			$default = get(array_keys($this->tables), 0);
+			return get(array_keys($this->tables), 0);
+	}
+	public function buildWhere($default) {
+		$params = array();
+		$r = static::processConditions($this->where, 'and', false, $default);
+		if($r[0])
+			return array(' WHERE '.$r[0], $r[1]);
+		else
+			return array('', array());
+	}
+	public function buildGroupby() {
+		if($this->groupBy)
+			return ' GROUP BY '.$this->groupBy;
+	}
+	public function buildOrderby($default) {
+		if(!$this->orderBy)
+			return '';
+
+		$orderBy = ' ORDER BY ';
+		if(!is_array($this->orderBy))
+			$orders = array($this->orderBy);
+		else
+			$orders = $this->orderBy;
 		
-		if($this->orderBy) {
-			$orderBy = ' ORDER BY ';
-			if(!is_array($this->orderBy))
-				$orders = array($this->orderBy);
-			else
-				$orders = $this->orderBy;
-			
-			foreach($orders as $k=>$v)
-				if(preg_match('/^[a-zA-Z0-9_ ]+$/', $v))
-					$orders[$k] = $default.'.'.$v;
-					
-			$orderBy .= implode(', ', $orders);
-		}
+		foreach($orders as $k=>$v)
+			if(preg_match('/^[a-zA-Z0-9_ ]+$/', $v))
+				$orders[$k] = $default.'.'.$v;
 				
-		if($this->limit || $this->offset) {
-			$limit = ' LIMIT ';
-			if($this->offset) {
-				$limit .= $this->offset;
-				if($this->limit)
-					$limit .= ', '.$this->limit;
-				else
-					$limit .= ', 99999999';
-			}
-			else
-				$limit .= $this->limit;
-		}
-				
-		#todo put ` around table only, ie `like` l and not `like l`
-		foreach($this->rightjoin as $tableName=>$conditions) {
-			$table = $tableName;
-			if(preg_match('/^([a-zA-Z]+).Translation /', $tableName, $matches)) {
-				$ref_table = $this->getTable($matches[1]);
-				$table = preg_replace('/^([a-zA-Z]+)/', $ref_table, $table);
-				$table = str_replace('.Translation', '_translation', $table);
-			}
-			$r = static::processConditions($conditions);
-			$rightjoin .= ' RIGHT JOIN '.$table.' ON '.$r[0];
-			$params = array_merge($params, $r[1]);
-		}
-		
+		$orderBy .= implode(', ', $orders);
+		return $orderBy;
+	}
+	public function buildLeftjoin() {
+		$params = array();
+		$leftjoin = '';
 		foreach($this->leftjoin as $tableName=>$conditions) {
 			$table = $tableName;
 			if(preg_match('/^([a-zA-Z]+).Translation /', $tableName, $matches)) {
@@ -279,7 +285,27 @@ class DAL {
 			$leftjoin .= ' LEFT JOIN '.$table.' ON '.$r[0];
 			$params = array_merge($params, $r[1]);
 		}
-				
+		return array($leftjoin, $params);
+	}
+	public function buildRightjoin() {
+		$params = array();
+		$rightjoin = '';
+		foreach($this->rightjoin as $tableName=>$conditions) {
+			$table = $tableName;
+			if(preg_match('/^([a-zA-Z]+).Translation /', $tableName, $matches)) {
+				$ref_table = $this->getTable($matches[1]);
+				$table = preg_replace('/^([a-zA-Z]+)/', $ref_table, $table);
+				$table = str_replace('.Translation', '_translation', $table);
+			}
+			$r = static::processConditions($conditions);
+			$rightjoin .= ' RIGHT JOIN '.$table.' ON '.$r[0];
+			$params = array_merge($params, $r[1]);
+		}
+		return array($rightjoin, $params);
+	}
+	public function buildInnerjoin() {
+		$params = array();
+		$innerjoin = '';
 		foreach($this->innerjoin as $tableName=>$conditions) {
 			$table = $tableName;
 			if(preg_match('/^([a-zA-Z]+).Translation /', $tableName, $matches)) {
@@ -292,46 +318,60 @@ class DAL {
 			$innerjoin .= ' INNER JOIN '.$table.' ON '.$r[0];
 			$params = array_merge($params, $r[1]);
 		}
-		
-		$r = static::processConditions($this->where, 'and', false, $default);
-		if($where = $r[0]) {
-			$where = ' WHERE '.$where;
-			$params = array_merge($params, $r[1]);
+		return array($innerjoin, $params);
+	}
+	public function buildLimit() {
+		if(!$this->limit && !$this->offset)
+			return '';
+
+		$limit = ' LIMIT ';
+		if($this->offset) {
+			$limit .= $this->offset;
+			if($this->limit)
+				$limit .= ', '.$this->limit;
+			else
+				$limit .= ', 99999999';
 		}
+		else
+			$limit .= $this->limit;
+		return $limit;
+	}
 
-		if($this->groupBy)
-			$groupBy = ' GROUP BY '.$this->groupBy;
-
-		return array('SELECT * FROM '.$sqltable.$rightjoin.$leftjoin.$innerjoin.$where.$orderBy.$limit, $params);
-	}
-	
-	public function first() {
-		list($sql, $params) = $this->limit(1)->buildSQL();
-		return $this->db->query($sql, $params)->first();
-	}
-	
-	public function get() {
-		list($sql, $params) = $this->buildSQL();
-		return $this->db->query($sql, $params, $params)->all();
-	}
-	
-	public function paginate($page, $per_page=10) {
-		$page = $page ? $page:1;
-		$this->offset(($page-1)*$per_page);
-		$this->limit($per_page);
+	public function buildSQL() {
+		$params = array();
 		
-		return $this;
+		$tables = $this->buildTables();
+		
+		$default = $this->getDefaultTable();
+		
+		$orderBy = $this->buildorderby($default);
+				
+		$limit = $this->buildLimit();
+				
+		#todo put ` around table only, ie `like` l and not `like l`
+		list($rightjoin, $rjparams) = $this->buildRightjoin();
+		$params = array_merge($params, $rjparams);
+		
+		list($leftjoin, $ljparams) = $this->buildLeftjoin();
+		$params = array_merge($params, $ljparams);
+				
+		list($innerjoin, $ijparams) = $this->buildInnerjoin();
+		$params = array_merge($params, $ijparams);
+		
+		list($where, $whereparams) = $this->buildWhere($default);
+		$params = array_merge($params, $whereparams);
+
+		$groupby = $this->buildGroupby();
+
+		return array('SELECT * FROM '.$tables.$rightjoin.$leftjoin.$innerjoin.$where.$groupby.$orderBy.$limit, $params);
 	}
 	
+	/* FUNCTIONS */
 	public function update($values) {
-		$where = '';
 		$params = array();
 
-		$r = static::processConditions($this->where);
-		if($where = $r[0]) {
- 			$where = ' WHERE '.$where;
-			$params = array_merge($params, $r[1]);
-		}
+		list($where, $whereparams) = $this->buildWhere($default);
+		$params = array_merge($params, $whereparams);
 		
 		if(sizeof($values) == 0)
 			throw new Exception('Update values should not be empty.');
@@ -369,183 +409,62 @@ class DAL {
 	}
 	
 	public function delete() {
-		$where = '';
 		$params = array();
 		
 		$table = get(array_keys($this->tables), 0);
 
-		$r = static::processConditions($this->where);
-		if($where = $r[0]) {
- 			$where = ' WHERE '.$where;
-			$params = array_merge($params, $r[1]);
-		}
+		list($where, $whereparams) = $this->buildWhere($default);
+		$params = array_merge($params, $whereparams);
 	
 		$sql = 'DELETE FROM '.$table.$where;
 		
 		return $this->db->query($sql, $params)->affected();
 	}
-	
-	public function count($group_by=null) {
-		$where = '';
+
+	protected function _function($fct, $what=null, $group_by=null) {
 		$params = array();
 
-		$r = static::processConditions($this->where);
-		if($where = $r[0]) {
- 			$where = ' WHERE '.$where;
-			$params = array_merge($params, $r[1]);
-		}
+		list($where, $whereparams) = $this->buildWhere($default);
+		$params = array_merge($params, $whereparams);
 	
-		$tables = array();
-		foreach($this->tables as $table=>$alias)
-			if($alias)
-				$tables[] = '`'.$table.'` '.$alias;
-			else
-				$tables[] = '`'.$table.'`';
-		$sqltable = implode(', ', $tables);
-		
+		$tables = $this->buildTables();
+	
+		if($what)
+			$what = '`'.$what.'`';
+		else
+			$what = '*';
+
 		if($group_by) {
-			$sql = 'SELECT `'.$group_by.'` as groupby, count(*) as total FROM '.$sqltable.$where.' GROUP BY '.$group_by;
+			$sql = 'SELECT `'.$group_by.'` as groupby, '.$fct.'('.$what.') as '.$fct.' FROM `'.$this->table.'`'.$where.' GROUP BY '.$group_by;#todo $this->table
 			$res = array();
 			foreach($this->db->query($sql, $params)->all() as $v)
-				$res[$v['groupby']] = $v['total'];
+				$res[$v['groupby']] = $v[$fct];
 			return $res;
 		}
 		else {
-			$sql = 'SELECT count(*) as total FROM '.$sqltable.$where;
+			$sql = 'SELECT '.$fct.'('.$what.') as '.$fct.' FROM '.$tables.$where;
 			$res = $this->db->query($sql, $params)->first();
-			return $res['total'];
+			return $res[$fct];
 		}
+	}
+	
+	public function count($group_by=null) {
+		return $this->_function('count', null, $group_by);
 	}
 	
 	public function min($what, $group_by=null) {
-		$where = '';
-		$params = array();
-
-		$r = static::processConditions($this->where);
-
-		if($where = $r[0]) {
-			$where = ' WHERE '.$where;
-			$params = array_merge($params, $r[1]);
-		}
-		
-		$tables = array();
-		foreach($this->tables as $table=>$alias)
-			if($alias)
-				$tables[] = $table.' '.$alias;
-			else
-				$tables[] = $table;
-		$sqltable = implode(', ', $tables);
-	
-		if($group_by) {
-			$sql = 'SELECT `'.$group_by.'` as groupby, min(`'.$what.'`) as min FROM `'.$this->table.'`'.$where.' GROUP BY '.$group_by;
-			$res = array();
-			foreach($this->db->query($sql, $params)->all() as $v)
-				$res[$v['groupby']] = $v['min'];
-			return $res;
-		}
-		else {
-			$sql = 'SELECT min(`'.$what.'`) as min FROM '.$sqltable.$where;
-			$res = $this->db->query($sql, $params)->first();
-			return $res['min'];
-		}
+		return $this->_function('min', $what, $group_by);
 	}
 	
 	public function max($what, $group_by=null) {
-		$where = '';
-		$params = array();
-
-		$r = static::processConditions($this->where);
-
-		if($where = $r[0]) {
-			$where = ' WHERE '.$where;
-			$params = array_merge($params, $r[1]);
-		}
-		
-		$tables = array();
-		foreach($this->tables as $table=>$alias)
-			if($alias)
-				$tables[] = $table.' '.$alias;
-			else
-				$tables[] = $table;
-		$sqltable = implode(', ', $tables);
-	
-		if($group_by) {
-			$sql = 'SELECT `'.$group_by.'` as groupby, max(`'.$what.'`) as max FROM `'.$this->table.'`'.$where.' GROUP BY '.$group_by;
-			$res = array();
-			foreach($this->db->query($sql, $params)->all() as $v)
-				$res[$v['groupby']] = $v['max'];
-			return $res;
-		}
-		else {
-			$sql = 'SELECT max(`'.$what.'`) as max FROM '.$sqltable.$where;
-			$res = $this->db->query($sql, $params)->first();
-			return $res['max'];
-		}
+		return $this->_function('max', $what, $group_by);
 	}
 	
 	public function avg($what, $group_by=null) {
-		$where = '';
-		$params = array();
-
-		$r = static::processConditions($this->where);
-
-		if($where = $r[0]) {
-			$where = ' WHERE '.$where;
-			$params = array_merge($params, $r[1]);
-		}
-		
-		$tables = array();
-		foreach($this->tables as $table=>$alias)
-			if($alias)
-				$tables[] = $table.' '.$alias;
-			else
-				$tables[] = $table;
-		$sqltable = implode(', ', $tables);
-	
-		if($group_by) {
-			$sql = 'SELECT `'.$group_by.'` as groupby, avg(`'.$what.'`) as avg FROM `'.$this->table.'`'.$where.' GROUP BY '.$group_by;
-			$res = array();
-			foreach($this->db->query($sql, $params)->all() as $v)
-				$res[$v['groupby']] = $v['avg'];
-			return $res;
-		}
-		else {
-			$sql = 'SELECT avg(`'.$what.'`) as avg FROM '.$sqltable.$where;
-			$res = $this->db->query($sql, $params)->first();
-			return $res['avg'];
-		}
+		return $this->_function('avg', $what, $group_by);
 	}
 	
 	public function sum($what, $group_by=null) {
-		$where = '';
-		$params = array();
-
-		$r = static::processConditions($this->where);
-
-		if($where = $r[0]) {
-			$where = ' WHERE '.$where;
-			$params = array_merge($params, $r[1]);
-		}
-		
-		$tables = array();
-		foreach($this->tables as $table=>$alias)
-			if($alias)
-				$tables[] = $table.' '.$alias;
-			else
-				$tables[] = $table;
-		$sqltable = implode(', ', $tables);
-	
-		if($group_by) {
-			$sql = 'SELECT `'.$group_by.'` as groupby, sum(`'.$what.'`) as sum FROM `'.$this->table.'`'.$where.' GROUP BY '.$group_by;
-			$res = array();
-			foreach($this->db->query($sql, $params)->all() as $v)
-				$res[$v['groupby']] = $v['sum'];
-			return $res;
-		}
-		else {
-			$sql = 'SELECT sum(`'.$what.'`) as sum FROM '.$sqltable.$where;
-			$res = $this->db->query($sql, $params)->first();
-			return $res['sum'];
-		}
+		return $this->_function('sum', $what, $group_by);
 	}
 }
