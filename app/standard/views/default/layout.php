14ac1d18fb5f5226261258e76aecbad839224b6e
Michel <leyou.m@gmail.com>
2013-01-04 20:43:40 +0100

    default index css/js

--- app/standard/views/default/layout.php
+++ app/standard/views/default/layout.php
@@ -1,8 +1,3 @@
-<?php HTML::include_js('js/test1.js') ?>
-<?php HTML::include_js('js/test2.js') ?>
-<?php HTML::include_css('http://static.jquery.com/files/rocker/css/reset.css') ?>
-<?php HTML::include_css('bundles/admin/css/default.css') ?>
-<?php HTML::include_css('bundles/admin/css/admin.css') ?>
 <h1>Coxis</h1>
 <div>
 	<?php echo $content ?>
