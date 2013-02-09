2fc8b9266f8a74348093a1f90733e50243425597
Michel <leyou.m@gmail.com>
2013-02-09 23:00:49 +0100

    use form_editable

--- coxis/form/ModelForm.php
+++ coxis/form/ModelForm.php
@@ -79,8 +79,7 @@ class ModelForm extends Form {
 					continue;
 			if(isset($params['except']) && in_array($name, $params['except']))
 					continue;
-			// if($properties->form['hidden'] === false && !(isset($params['with']) && in_array($name, $params['with'])))
-			if($properties->editable === false)
+			if($properties->form_editable === false)
 				continue;
 
 			if($this->i18n && $properties->i18n) {
