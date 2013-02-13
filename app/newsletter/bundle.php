f435db67452a1808b8e408b73c3116e7ac3fe3e4
Michel <leyou.m@gmail.com>
2013-02-13 16:57:45 +0100

    page icon and subscriber typo

--- app/newsletter/bundle.php
+++ app/newsletter/bundle.php
@@ -3,6 +3,6 @@ AdminMenu::$menu[0]['childs'][] = array('label' => 'Newsletter', 'link' => 'news
 
 \Coxis\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('newsletter/icon.svg'), 'link'=>'newsletter', 'title' => __('Newsletter'), 'description' => 'Send mailings to your subscribers!');
 
-AdminMenu::$menu[0]['childs'][] = array('label' => 'Sbuscribers', 'link' => 'subscribers');
+AdminMenu::$menu[0]['childs'][] = array('label' => 'Subscribers', 'link' => 'subscribers');
 
 \Coxis\Admin\Libs\AdminMenu::$home[] = array('img'=>\URL::to('newsletter/subscribers-icon.svg'), 'link'=>'subscribers', 'title' => __('Subscribers'), 'description' => 'All your newsletter subscribers.');
