62bc44ca14e2d786806190f31495d7bf9d42033e
unknown <leyou.m@gmail.com>
2013-11-18 23:41:35 +1100

    fixes

--- app/Bundle.php
+++ app/Bundle.php
@@ -3,12 +3,10 @@ namespace App;
 
 class Bundle extends \Coxis\Core\BundleLoader {
 	public function load($queue) {
-		foreach(glob(dirname(__FILE__).'/*', GLOB_ONLYDIR) as $d)
-			$queue->addBundle($d);
+		$queue->addBundles(glob(dirname(__FILE__).'/*', GLOB_ONLYDIR));
 		parent::load($queue);
 	}
 
 	public function run() {
 	}
 }
-return new Bundle;
