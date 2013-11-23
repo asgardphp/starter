7813c19f4a6cdb580eea25ec90160fe9fe1fa030
unknown <leyou.m@gmail.com>
2013-11-23 18:37:03 +1100

    utils functions in load.php and updated config

--- app/load.php
+++ app/load.php
@@ -1,1 +1,21 @@
 <?php
+if(!function_exists('d')) {
+	function d() {
+		call_user_func_array(array('Coxis\Utils\Debug', 'd'), func_get_args());
+	}
+}
+if(!function_exists('__')) {
+	function __($key, $params=array()) {
+		return \Coxis\Core\Context::get('locale')->translate($key, $params);
+	}
+}
+if(!function_exists('from')) {
+	function from($from='') {
+		return new \Coxis\Core\Importer($from);
+	}
+}
+if(!function_exists('import')) {
+	function import($what, $into='') {
+		return from()->import($what, $into);
+	}
+}
