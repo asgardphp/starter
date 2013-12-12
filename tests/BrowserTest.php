f8a08abe593ee6091e5b311e4f5cb3189b287c86
Michel Hognerud <leyou.m@gmail.com>
2013-12-12 15:40:13 +1100

    replace php-query with xpath

--- tests/BrowserTest.php
+++ tests/BrowserTest.php
@@ -4,16 +4,6 @@ if(!defined('_ENV_'))
 require_once(_CORE_DIR_.'core.php');
 \Coxis\Core\Coxis::load();
 
-require_once _VENDOR_DIR_.'duvanmonsa/php-query/src/phpQuery.php';
-
-if(!function_exists('_pq')) {
-	function _pq($html, $selector) {
-		$doc = phpQuery::newDocument($html);
-		phpQuery::selectDocument($doc);
-		return pq($selector);
-	}
-}
-
 class BrowserTest extends PHPUnit_Framework_TestCase {
 	public function setUp(){
 		\DB::import('tests/coxis.sql');
@@ -26,6 +16,7 @@ class BrowserTest extends PHPUnit_Framework_TestCase {
 
 	public function test1() {
 		$browser = new \Coxis\Utils\Browser;
-		$this->assertEquals(_pq($browser->get('')->content, 'h1')->html(), 'Coxis');
+		$doc = new Coxis\Xpath\Doc($browser->get('')->content);
+		$this->assertEquals($doc->text('//h1'), 'Coxis');
 	}
 }
