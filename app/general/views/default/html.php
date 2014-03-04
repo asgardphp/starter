aa1e28f1227d90e6f6bc620519aca69ef2bff566
Michel Hognerud <leyou.m@gmail.com>
2014-03-04 13:18:45 +0800

    fixes

--- app/general/views/default/html.php
+++ app/general/views/default/html.php
@@ -3,15 +3,15 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<!-- <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> -->
-	<?php \Coxis\Core\App::get('html')->show_title() ?>
-	<?php \Coxis\Core\App::get('html')->show_description() ?>
-	<?php \Coxis\Core\App::get('html')->show_keywords() ?>
+	<?php \Coxis\Core\App::get('html')->printTitle() ?>
+	<?php \Coxis\Core\App::get('html')->printDescription() ?>
+	<?php \Coxis\Core\App::get('html')->printKeywords() ?>
 	<base href="<?php echo \Coxis\Core\App::get('url')->base() ?>" />
-	<?php \Coxis\Core\App::get('html')->show_include_js() ?>
-	<?php \Coxis\Core\App::get('html')->show_include_css() ?>
-	<?php \Coxis\Core\App::get('html')->show_code_js() ?>
-	<?php \Coxis\Core\App::get('html')->show_code_css() ?>
-	<?php \Coxis\Core\App::get('html')->show_code() ?>
+	<?php \Coxis\Core\App::get('html')->printJSInclude() ?>
+	<?php \Coxis\Core\App::get('html')->printCSSInclude() ?>
+	<?php \Coxis\Core\App::get('html')->printJSCode() ?>
+	<?php \Coxis\Core\App::get('html')->printCSSCode() ?>
+	<?php \Coxis\Core\App::get('html')->printCode() ?>
 </head>
 <body>
 	<?php echo $content ?>
