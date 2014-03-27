10af27c1e71f1d10eed0fdc5c20a86a2e07072ba
Michel Hognerud <leyou.m@gmail.com>
2014-03-27 10:44:06 +0800

    autoloader and composer script

--- app/load.php
+++ app/load.php
@@ -9,19 +9,16 @@ if(!function_exists('__')) {
 		return \Asgard\Core\App::get('locale')->translate($key, $params);
 	}
 }
-if(!function_exists('from')) {
-	function from($from='') {
-		return new \Asgard\Core\Importer($from);
-	}
-}
-if(!function_exists('import')) {
-	function import($what, $into='') {
-		return from()->import($what, $into);
-	}
-}
 
-\Asgard\Core\Autoloader::$directories['Asgard'] = 'bundles';
-\Asgard\Core\Autoloader::$directories['Psr\Log'] = 'log/Psr/Log';
+\Asgard\Core\App::register('autoloader', function() {
+	$autoloader = new \Asgard\Core\Autoloader;
+	$autoloader->globalNamespace(\Asgard\Core\App::get('config')->get('global_namespace'));
+	$autoloader->preload(\Asgard\Core\App::get('config')->get('preload'));
+	return $autoloader;
+});
+spl_autoload_register(array(\Asgard\Core\App::get('autoloader'), 'autoload')); #asgard autoloader
+\Asgard\Core\App::get('autoloader')->namespaceMap('Asgard', 'bundles');
+\Asgard\Core\App::get('autoloader')->namespaceMap('Psr\Log', 'log/Psr/Log');
 \Asgard\Core\App::set('logger', function() {
 	return new \App\Logger;
 });
