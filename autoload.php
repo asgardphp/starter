5b724f6ec939678ea9274067cf6b714561c5e4cf
Michel Hognerud <leyou.m@gmail.com>
2014-09-05 14:58:07 +0200

    fixing autoloader

--- autoload.php
+++ autoload.php
@@ -1,8 +1,4 @@
 <?php
-require 'vendor/autoload.php';
-
-foreach(spl_autoload_functions() as $function) {
-	if(is_array($function) && $function[0] instanceof \Composer\Autoload\ClassLoader)
-		$function[0]->setUseIncludePath(true);
-}
+$composer = require 'vendor/autoload.php';
+$composer->setUseIncludePath(true);
 set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__.'/app');
