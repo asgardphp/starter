78d689aad6c10de48953167d35f14de9e2345971
Michel <leyou.m@gmail.com>
2015-06-05 15:23:32 +0200

    fix console + remove cache from index.php

--- web/index.php
+++ web/index.php
@@ -7,7 +7,5 @@ require_once '../autoload.php'; #composer autoloader
 
 /* RUN & SEND */
 $kernel = new Kernel();
-if($kernel->getEnv() === 'prod')
-	$kernel->setCache(new Doctrine\Common\Cache\FilesystemCache('../storage/cache'));
 $kernel->load();
 $kernel->getContainer()['httpKernel']->run()->send();
