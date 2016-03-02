6eebe7d23ca2c59772c197d4f7a92df7af8dea01
michel hognerud <leyou.m@gmail.com>
2016-03-02 15:01:58 +0100

    remove gzip compression. should be done through apache/nginx.

--- app/General/Hooks/GeneralHooks.php
+++ app/General/Hooks/GeneralHooks.php
@@ -19,21 +19,4 @@ class GeneralHooks extends \Asgard\Hook\HookContainer {
 	public static function hook404Exception(\Asgard\Hook\Chain $chain, \Exception $exception, \Asgard\Http\Response &$response, \Asgard\Http\Request $request) {
 		$response = $chain->getContainer()['httpKernel']->runController('General\Controllers\DefaultController', '_404', $request)->setCode(404);
 	}
-
-	/**
-	 * @Hook("Asgard.Http.Output")
-	 * @Priority(1000)
-	 */
-	public static function gzip(\Asgard\Hook\Chain $chain, \Asgard\Http\Response $response, \Asgard\Http\Request $request) {
-		if(!strstr($request->server['HTTP_ACCEPT_ENCODING'], 'gzip') || !$response->getContent())
-			return;
-
-		$output = gzencode($response->getContent());
-		$response
-			->setContent($output)
-			->setHeader('Content-Encoding', 'gzip')
-			->setHeader('Content-Length', strlen($output))
-			->setHeader('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
-			->setHeader('Pragma', 'no-cache');
-	}
 }
