2a1fa7199f52192c44477a47fc7757ce4a3dacd6
michel hognerud <leyou.m@gmail.com>
2016-03-16 17:17:06 +0100

    consistent plural package naming

--- app/bootstrap_all.php
+++ app/bootstrap_all.php
@@ -43,7 +43,7 @@ $container['hooks']->hook('Asgard.Entity.Definition', function($chain, $definiti
 $container['httpKernel']->start($container['kernel']->get('root').'/app/start.php');
 
 #Layout
-$container['httpKernel']->filterAll('Asgard\Http\Filters\PageLayout', [
+$container['httpKernel']->filterAll('Asgard\Http\Filter\PageLayout', [
 	['\General\Controllers\DefaultController', 'layout'],
 	$container['kernel']->get('root').'/app/General/html/html.php'
 ]);
